7785aa424a3a8ee7a91bf0bffe1581e7805e2dbb
llschuster <perez99leo@outlook.es>
2020-11-01 20:41:30 +0100

    Added game feedback

--- app/src/main/java/llschuster/languagegame/de/MainActivity.kt
+++ app/src/main/java/llschuster/languagegame/de/MainActivity.kt
@@ -2,6 +2,7 @@ package llschuster.languagegame.de
 
 import android.animation.Animator
 import android.animation.ObjectAnimator
+import android.graphics.Color
 import android.graphics.Point
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -31,6 +32,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var denyBtn: View
     lateinit var progressBar: ProgressBar
     lateinit var gameScoreTxt: TextView
+    lateinit var feedbackTxt: TextView
     var displaySize: Point = Point()
     private val playScreenViewModel: PlayScreenViewModel by viewModels()
     var animationFinalValue = 800f
@@ -68,6 +70,33 @@ class MainActivity : AppCompatActivity() {
             this.duration = 4800
         }
         animatedProgressBar.start()
+    }
+
+    fun animateFeedback(text: String, color: Int){
+        feedbackTxt.setText(text)
+        feedbackTxt.setTextColor(color)
+        var animatedFeebackTxt = ObjectAnimator.ofFloat(feedbackTxt, "alpha", 0.5f, 1.0f).apply {
+            this.duration = 1500
+            this.addListener(object : Animator.AnimatorListener{
+                override fun onAnimationRepeat(animation: Animator?) {
+                    return
+                }
+
+                override fun onAnimationEnd(animation: Animator?) {
+                    feedbackTxt.visibility = View.INVISIBLE
+                }
+
+                override fun onAnimationCancel(animation: Animator?) {
+                    return
+                }
+
+                override fun onAnimationStart(animation: Animator?) {
+                    feedbackTxt.visibility = View.VISIBLE
+                }
+
+            })
+        }
+        animatedFeebackTxt.start()
     }
 
     fun inflateWord(text: String): View?{
@@ -115,11 +144,15 @@ class MainActivity : AppCompatActivity() {
         confirmBtn = findViewById(R.id.correctBtn)
         denyBtn = findViewById(R.id.incorrectBtn)
         gameScoreTxt = findViewById(R.id.gameScoreTxt)
+        feedbackTxt = findViewById(R.id.feedbackTxt)
         progressBar = findViewById(R.id.progressBar)
 
         confirmBtn.setOnClickListener {
             var confirmResult = playScreenViewModel.confirmMatch()
             decision = if (confirmResult) DecisionOutcomeType.match else DecisionOutcomeType.skip
+            var feedbackText = if (confirmResult) "Well done" else "Bad luck"
+            var feedbackColor = if (confirmResult) Color.GREEN else Color.RED
+            animateFeedback(feedbackText, feedbackColor)
             animatedWord?.objectAnim?.cancel()
         }
 
@@ -127,6 +160,7 @@ class MainActivity : AppCompatActivity() {
             try {
                 var denyResult = playScreenViewModel.denyMatch()
                 decision = if (!denyResult) DecisionOutcomeType.match else DecisionOutcomeType.skip
+                if (!denyResult) animateFeedback("That was actually right", Color.RED)
                 animatedWord?.objectAnim?.cancel()
             } catch (e: Exception){
                 Log.i("error", e.toString())
@@ -134,7 +168,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         windowManager.defaultDisplay.getSize(displaySize)
-        animationFinalValue = displaySize.y * 1.10f
+        animationFinalValue = displaySize.y * 1.05f
 
         playScreenViewModel.wordListInputStream = assets.open("words_v2.json")
         playScreenViewModel.getListOfWords()
@@ -153,10 +187,6 @@ class MainActivity : AppCompatActivity() {
         playScreenViewModel.gameScore.observe(this) {
             gameScoreTxt.setText(it.toString())
         }
-    }
-
-    override fun onResume() {
-        super.onResume()
     }
 }
 
